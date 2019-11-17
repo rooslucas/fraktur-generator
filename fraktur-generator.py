f5cf955a78d72db7c1c4eed325526c014c8646cd
rooslucas <rooslucas@live.nl>
2019-11-17 17:43:57 +0100

    Use command arguments as input

--- fraktur-generator.py
+++ fraktur-generator.py
@@ -1,7 +1,9 @@
+import sys
+
 fraktur = '𝕬𝕭𝕮𝕯𝕰𝕱𝕲𝕳𝕴𝕵𝕶𝕷𝕸𝕹𝕺𝕻𝕼𝕽𝕾𝕿𝖀𝖁𝖂𝖃𝖄𝖅𝖆𝖇𝖈𝖉𝖊𝖋𝖌𝖍𝖎𝖏𝖐𝖑𝖒𝖓𝖔𝖕𝖖𝖗𝖘𝖙𝖚𝖛𝖜𝖝𝖞𝖟 .,?!'
 normal = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz .,?!'
 
-mySentence = input()
+mySentence = sys.argv[1]
 result = ''
 
 for character in mySentence:
